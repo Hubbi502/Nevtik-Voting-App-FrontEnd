369fca6432b9fda9f8c487b18f3616f4eaac7108
Muhammad Hubbi El Fairuz <89328047+Hubbi502@users.noreply.github.com>
2025-04-18 16:52:50 +0700

    Update config.ts

--- src/lib/api/config.ts
+++ src/lib/api/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'nevtik-voting-app-backend-production.up.railway.app';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://nevtik-voting-app-backend-production.up.railway.app';
 
 export const defaultHeaders = {
   'Content-Type': 'application/json',
